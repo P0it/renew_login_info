647027a52a9a2eb035304af39d6dc0485ebb6772
SnetHyunwoo <hyunwoo.jung@snetsystems.co.kr>
2023-01-26 15:32:24 +0900

    add package , remove files

--- renewer/main.py
+++ renewer/main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import yaml
 
-data = pd.read_csv('D:/PycharmWorkspace/renewer/test.csv')
+data = pd.read_csv('D:/PycharmWorkspace/renewer/src/test.csv')
 print(data.head())
 
 
-with open('dev.yaml_template') as f:
+with open('src/dev.yaml_template') as f:
     film = yaml.load(f, Loader=yaml.FullLoader)
     pd.options.dis
 
