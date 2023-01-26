0205a8550ef58c040aeb05917c839fdeb050240d
SnetHyunwoo <hyunwoo.jung@snetsystems.co.kr>
2023-01-26 17:22:30 +0900

    pyCharm git test

--- renewer/main.py
+++ renewer/main.py
@@ -1,12 +1,15 @@
 import pandas as pd
 import yaml
 
-data = pd.read_csv('D:/PycharmWorkspace/renewer/src/test.csv')
+data = pd.read_csv('src/test.csv')
 print(data.head())
 
 
 with open('src/dev.yaml_template') as f:
     film = yaml.load(f, Loader=yaml.FullLoader)
-    pd.options.dis
+
+
+
+
 
 
