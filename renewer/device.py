ce8b641d0b4f7cb2ea99f29539c74c2f6175fc73
poit <taesion9060@gmail.com>
2023-01-27 16:55:20 +0900

    setter, getter

--- renewer/device.py
+++ renewer/device.py
@@ -1,23 +1,37 @@
 class DeviceInfo:
-    def __init__(self, hostname, ssh_port, ssh_id, ssh_pwd, enable):
-        self.hostname = hostname
-        self.ssh_port = ssh_port
-        self.ssh_id = ssh_id
-        self.ssh_pwd = ssh_pwd
-        self.enable = enable
+    def __init__(self):
+        self.hostname = None
+        self.ssh_port = None
+        self.ssh_id = None
+        self.ssh_pwd = None
+        self.enable = None
 
     def set_host(self, hostname):
         self.hostname = hostname
 
+    def get_host(self):
+        return self.hostname
+
     def set_port(self, port):
         self.ssh_port = port
+
+    def get_port(self):
+        return self.ssh_port
 
     def set_id(self, id):
         self.ssh_id = id
 
+    def get_id(self):
+        return self.ssh_id
+
     def set_pwd(self, pwd):
         self.ssh_pwd = pwd
+
+    def get_pwd(self):
+        return self.ssh_pwd
 
     def set_enable(self, enable):
         self.enable = enable
 
+    def get_enable(self):
+        return self.enable
--- renewer/main.py
+++ renewer/main.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import yaml
+import sys
 from device import DeviceInfo
 
 
-# 클래스 생성(상속)
+# class( inherit )
 class FirstDevice(DeviceInfo):
     pass
 
@@ -12,45 +13,63 @@ class SecondDevice(DeviceInfo):
     pass
 
 
-data = pd.read_csv('src/test.csv')
+def renew_login():
+    # csv 파일 읽기
+    data = pd.read_csv(file_path)
+    print(data)
 
-for item in data.values:
-    if item[0] == '10.10.249.20':
-        FirstDevice.set_host(item[1])
-        FirstDevice.set_port(item[2])
-        FirstDevice.set_id(item[3])
-        FirstDevice.set_pwd(item[4])
-        FirstDevice.set_enable(item[5])
+    # 클래스 생성자 초기화
+    fd = FirstDevice()
+    sd = SecondDevice()
+    # 갱신 정보 객체에 저장
+    for item in data.values:
+        if item[0] == '10.10.249.20':
+            fd.set_host(item[1])
+            fd.set_port(item[2])
+            fd.set_id(item[3])
+            fd.set_pwd(item[4])
+            fd.set_enable(item[5])
 
-    if item[0] == '10.10.250.22':
-        SecondDevice.set_port(item[1])
-        SecondDevice.set_host(item[2])
-        SecondDevice.set_id(item[3])
-        SecondDevice.set_pwd(item[4])
-        SecondDevice.set_enable(item[5])
+        if item[0] == '10.10.250.22':
+            sd.set_host(item[1])
+            sd.set_port(item[2])
+            sd.set_id(item[3])
+            sd.set_pwd(item[4])
+            sd.set_enable(item[5])
 
-    else:
-        pass
+        else:
+            pass
 
-    print(item)
+    with open('src/dev.yaml_template') as f:
+        list_doc = yaml.safe_load(f)
+        device_list = list_doc['devices']
 
-with open('src/dev.yaml_template') as f:
-    list_doc = yaml.safe_load(f)
-    device_list = list_doc['devices']
+    for device in device_list:
 
-print("---")
-print(list_doc)
+        if device == "OL_L3_48T":
+            pass
+        # device['type'] = "router"
 
-print(type(list_doc))
-print(type(device_list))
+    with open('src/dev.yaml_template', "w") as f:
+        yaml.dump(list_doc, f)
 
-print(list_doc['devices']['OL_L3_48T']['type'])
 
-for device in device_list:
-    print(type(device))
-    if device == "OL_L3_48T":
-        pass
-    # device['type'] = "router"
+# 메인 실행 함수
+if len(sys.argv) < 2:
+    print("--- Downloading ---")
+    # TODO file_path 테스트 후에 삭제
+    file_path = 'src/test.csv'
+    renew_login()
+    # method: yaml -> csv
 
-with open('src/dev.yaml_template', "w") as f:
-    yaml.dump(list_doc, f)
+
+elif len(sys.argv) == 2:
+    print("--- Renewing ---")
+    # method: csv -> yaml
+    # TODO file_path 테스트 후에 원복
+    # file_path = sys.argv[1]
+    file_path = 'src/test.csv'
+    renew_login()
+
+else:
+    print("Only single csv file is required.")
